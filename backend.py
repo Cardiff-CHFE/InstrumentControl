843539564739d3973f3d8a02135b22c2928c0f0f
Samuel Partridge <partridgeSL@cardiff.ac.uk>
2017-11-01 13:32:35 +0000

    Schema and simplified loading

--- backend.py
+++ backend.py
@@ -1,67 +1,74 @@
 import json
-import scpi
-import visa
 import time
 import csv
 import os
 import os.path
+from schema import TObject, TDict, TUnion, TList, TString, TFloat
+
+
+class ConfigLoader():
+    def __init__(self):
+        class Configuration(TObject):
+            datadir = TString()
+            instruments = TDict(TUnion({}))
+            record_duration = TFloat()
+            samples = TList(TString())
+
+        self.schema = Configuration
+
+    def register_instrument(self, name, dtype):
+        instrument_dict = self.schema.dtypes['instruments']
+        union = instrument_dict.dtype
+        union.dtypes[name] = dtype
+
+    def load(self, data):
+        return self.schema(data)
+
+    def save(self, data):
+        return data.serialize()
 
 
 class InstrumentException(Exception):
     pass
 
+
 class Backend(object):
     def __init__(self):
         self.instrument_drivers = {}
         self.instruments = {}
-        self.configuration = {}
+        self.config = {}
         self.data_logger = DataLogger()
         self.logging = False
+        self.config_loader = ConfigLoader()
 
     def load_configfile(self, cfile):
         self.datadir = os.path.dirname(cfile)
         self.configfile = cfile
         with open(cfile) as fp:
             self.load_config(fp)
-        if not "instruments" in self.config:
-            self.config["instruments"] = {}
-        if not "record_duration" in self.config:
-            self.config["record_duration"] = 0.0
-        if "datadir" in self.config:
-            # Update data dir relative to config file (unless abs path used)
-            self.datadir = os.path.normpath(os.path.join(self.datadir, self.config["datadir"]))
 
     def save_configfile(self):
         with open(self.configfile, 'w') as fp:
             self.save_config(fp)
 
     def load_config(self, fp):
-        self.config = json.load(fp)
+        self.config = self.config_loader.load(json.load(fp))
+        if self.config.datadir != "":
+            # Update data dir relative to config file (unless abs path used)
+            self.datadir = os.path.normpath(os.path.join(self.datadir, self.config.datadir))
 
     def save_config(self, fp):
-        json.dump(self.config, fp, indent=4, separators=(',', ': '))
+        data = self.config_loader.save(self.config)
+        json.dump(data, fp, indent=4, separators=(',', ': '))
 
-    def load_instruments(self):
-        rm = visa.ResourceManager()
-        names = scpi.get_resource_names(rm)
-
-        for name, inst in self.config["instruments"].items():
-            driver_cls = self.instrument_drivers[inst["type"]]
-            ident = None
-            for iden, model in names.items():
-                if driver_cls.match_device(model):
-                    ident = iden
-                    break
-            if ident is None:
-                raise InstrumentException("Instrument of type '{}' not found".format(inst["type"]))
-
-            self.instruments[name] = driver_cls(rm.open_resource(ident))
+    def register_instrument(self, name, config, driver):
+        self.config_loader.register_instrument(name, config)
+        self.instrument_drivers[name] = driver
 
     def start(self):
-        self.load_instruments()
-
-        for name, inst in self.instruments.items():
-            inst.setup(self.config["instruments"][name])
+        for name, instcfg in self.config.instruments.items():
+            driver_cls = self.instrument_drivers[instcfg.type_]
+            self.instruments[name] = driver_cls(instcfg)
 
         stime = time.time()
         for name, inst in self.instruments.items():
@@ -70,8 +77,6 @@ class Backend(object):
     def stop(self):
         for inst in self.instruments.values():
             inst.stop()
-        for inst in self.instruments.values():
-            inst.cleanup()
         self.instruments = {}
 
     def start_logging(self, sample_name):
@@ -81,7 +86,7 @@ class Backend(object):
             if not existing:
                 self.data_logger.write(name, ["Time (s)"] + inst.get_headers())
         self.logging = True
-        if self.config["record_duration"] > 0.0:
+        if self.config.record_duration > 0.0:
             self.log_start = time.time()
         for inst in self.instruments.values():
             inst.on_record_start()
@@ -101,8 +106,8 @@ class Backend(object):
             if name in fns:
                 for s in samples:
                     fns[name](s[0], s[1])
-        if self.config["record_duration"] > 0.0 and self.logging:
-            if time.time() - self.log_start > self.config["record_duration"]:
+        if self.config.record_duration > 0.0 and self.logging:
+            if time.time() - self.log_start > self.config.record_duration:
                 self.stop_logging()
                 return False
         return True
@@ -118,7 +123,7 @@ class DataLogger(object):
         if not os.path.exists(path):
             os.makedirs(path)
         for name in names:
-            fname = os.path.join(path, name + ".csv")            
+            fname = os.path.join(path, name + ".csv")
             if os.path.isfile(fname):
                 exists.append(True)
             else:
