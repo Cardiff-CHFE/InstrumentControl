2d3eb80b4d1af64e0ec0a29aa462e7afaee78163
Samuel Partridge <partridgeSL@cardiff.ac.uk>
2017-12-13 20:29:24 +0000

    Select history length with spinbox and  type dropdown
    Type can be seconds/minutes etc

--- vna/dataWindow.py
+++ vna/dataWindow.py
@@ -37,8 +37,8 @@ class DataWindow(
         self.enabledSegments = [True]*segmentCount
 
         for i in range(segmentCount):
-            freq = self.frequencyPlot.plot(pen=colours[i])
-            qfac = self.qFactorPlot.plot(pen=colours[i])
+            freq = self.frequencyPlot.plot(pen=colours[i], connect='finite')
+            qfac = self.qFactorPlot.plot(pen=colours[i], connect='finite')
             self.frequencyPlotData.append(freq)
             self.qFactorPlotData.append(qfac)
 
@@ -58,18 +58,20 @@ class DataWindow(
 
         # Bind signals
         self.zeroOffset.clicked.connect(self.zeroOffsetClicked)
+        self.clearOffset.clicked.connect(self.clearOffsetClicked)
         self.bwOverride.stateChanged.connect(self.bwOverrideChanged)
         self.bwFactor.valueChanged.connect(self.bwFactorChanged)
         self.trackEnable.stateChanged.connect(self.trackEnableChanged)
         self.resetSegments.clicked.connect(self.resetSegmentsPressed)
         self.forceRetrack.clicked.connect(self.forceRetrackPressed)
         self.segmentTable.itemClicked.connect(self.segmentTableClicked)
-        self.historicalDuration.timeChanged.connect(self.historicalDurationChanged)
+
+        if self.instrument.cfg.sample_interval == 0.0:
+            self.displayDurationType.setCurrentText('samples')
+            self.displayDurationType.setEnabled(False)
 
         self.lastSample = None
         self.maxSamples = 100
-        self.historicalSeconds = 0
-        self.historicalDurationChanged(self.historicalDuration.time())
         self.frequencyBuffers = [array.array('f') for n in range(segmentCount)]
         self.qFactorBuffers = [array.array('f') for n in range(segmentCount)]
         self.timeBuffer = array.array('f')
@@ -81,9 +83,13 @@ class DataWindow(
             self.frequencyOffset = np.array(self.lastSample.f0, dtype=np.float)
             self.qFactorOffset = np.array(self.lastSample.q, dtype=np.float)
 
+    def clearOffsetClicked(self):
+        self.frequencyOffset = np.zeros(len(self.frequencyBuffers), dtype=np.float)
+        self.qFactorOffset = np.zeros(len(self.qFactorBuffers), dtype=np.float)
+
     def bwOverrideChanged(self, state):
         if state == Qt.Checked:
-            self.instrument.set_bw_factor_override(self.bw_factor.value())
+            self.instrument.set_bw_factor_override(self.bwFactor.value())
         else:
             self.instrument.set_bw_factor_override(None)
 
@@ -106,31 +112,46 @@ class DataWindow(
             row = self.segmentTable.row(tableItem)
             checked = tableItem.checkState() == Qt.Checked
             self.enabledSegments[row] = checked
-            if checked:
-                self.frequencyQueue[row] = np.nan
-                self.qFactorQueue[row] = np.nan
+            # if checked:
+            #     self.frequencyBuffer[row] = np.nan
+            #     self.qFactorBuffer[row] = np.nan
             self.instrument.set_segment_enabled(row, checked)
-
-    def historicalDurationChanged(self, time):
-        self.historicalSeconds = QTime(0,0).secsTo(time)
 
     def addSample(self, time, sample):
         for freq, frequencyBuffer in zip(sample.f0, self.frequencyBuffers):
-            frequencyBuffer.append(freq)
+            if freq is not None:
+                frequencyBuffer.append(freq)
+            else:
+                frequencyBuffer.append(np.nan)
 
         for qfac, qFactorBuffer in zip(sample.q, self.qFactorBuffers):
-            qFactorBuffer.append(qfac)
+            if qfac is not None:
+                qFactorBuffer.append(qfac)
+            else:
+                qFactorBuffer.append(np.nan)
 
         self.timeBuffer.append(time)
 
         self.lastSample = sample
 
     def refresh(self):
-        interval = self.instrument.cfg.sample_interval
         bufferlen = len(self.timeBuffer)
         if not bufferlen:
             return
-        start = max(bufferlen-int(self.historicalSeconds/interval), 0)
+
+        displayDuration = self.displayDuration.value()
+        displayType = self.displayDurationType.currentText()
+        if displayType == "samples":
+            start = max(bufferlen-max(int(displayDuration), 1)-1, 0)
+        else:
+            if displayType == "minutes":
+                displayDuration *= 60.0
+            elif displayType == "hours":
+                displayDuration *= 60.0*60.0
+            interval = self.instrument.cfg.sample_interval
+            start = max(bufferlen-int(displayDuration/interval)-1, 0)
+
+
         sample_skip = max((bufferlen-start)//self.maxSamples, 1)
         times = self.timeBuffer[start::sample_skip]
 
